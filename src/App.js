bd480c186ecb5d6640124075218b68ce1e8c549b
junipdewan <junipd2@gmail.com>
2019-03-03 16:13:25 +0530

    UI changes and code restructuring

--- src/App.js
+++ src/App.js
@@ -1,138 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 //import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
-import pf from 'petfinder-client';
+import { render } from "react-dom";
+//import pf from 'petfinder-client';
+import NavBar from "./NavBar";
+import RandomPet from "./RandomPet";
 
-import Pet from './Pet'
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-})
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       pets: []
-//     }
-//   }
-
-
-//   componentDidMount() {
-//     petfinder.pet.find({ output: "full", location: "Seattle, WA" })
-//       .then(data => {
-//         /**
-//          *  coming data from api may be single pet or array of pets 
-//          *  so check is enabled to know if it is an array or single pet
-//          *  element, accordingly update the state
-//          */
-//         let pets;
-//         let petfinderData = data.petfinder.pets;
-//         if (petfinderData && petfinderData.pet) {
-//           if (Array.isArray(petfinderData.pet)) {
-//             pets = petfinderData.pet;
-//           } else {
-//             pets = [petfinderData.pet];
-//           }
-//         } else {
-//           pets = [];
-//         }
-
-
-//       })
-
-//   }
-
-
-
-//   render() {
-//     return (
-//       <div>
-//         {this.state.pets.map(animal => {
-//           let breed;
-//           // breed may be one or two also
-//           if (Array.isArray(animal.breeds.breed)) {
-//             breed = animal.breeds.breed.join(', ');
-//           } else {
-//             breed = animal.breeds.breed;
-//           }
-//           return (
-//             <Pet
-//               key={animal.id}
-//               animal={animal.animal}
-//               name={animal.name}
-//               breed={breed}
-//               media={animal.media}
-//               location={`${animal.contact.city}, ${animal.contact.state}`}
-//             />
-//           )
-//         })}
-//       </div>
-//     )
-//   }
-// }
+const marginStyle = {
+  margin: '40px 0px 0px 0px'
+};
 
 function App() {
-  const [pets, updatePets] = useState([]);
-
-  function updatePetsData(pets_params) {
-    updatePets(pets_params)
-  }
-
-  /** fetch data on using petfinder api */
-  function fetchdata() {
-    petfinder.pet.find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        /**
-         *  coming data from api may be single pet or array of pets 
-         *  so check is enabled to know if it is an array or single pet
-         *  element, accordingly update the state
-         */
-        let pets;
-        let petfinderData = data.petfinder.pets;
-        if (petfinderData && petfinderData.pet) {
-          if (Array.isArray(petfinderData.pet)) {
-            pets = petfinderData.pet;
-          } else {
-            pets = [petfinderData.pet];
-          }
-        } else {
-          pets = [];
-        }
-        updatePetsData(pets);
-      })
-  }
-  // fetch data untill pets.lengths
-  useEffect(() => {
-    fetchdata();
-  }, [pets.length]);
-
   return (
-    <div >
-      {pets.map(animal => {
-        let breed;
-        // breed may be one or two also
-        if (Array.isArray(animal.breeds.breed)) {
-          breed = animal.breeds.breed.join(', ');
-        } else {
-          breed = animal.breeds.breed;
-        }
-        return (
-          <Pet
-            key={animal.id}
-            animal={animal.animal}
-            name={animal.name}
-            breed={breed}
-            media={animal.media}
-            location={`${animal.contact.city}, ${animal.contact.state}`}
-          />
-        )
-      })}
-    </div >
-  )
+    <div>
+      <NavBar />
+      <div style={marginStyle}>
+        <RandomPet />
+      </div>
+      
+    </div>
+  );
 }
 
 let rootElement = document.getElementById("root");

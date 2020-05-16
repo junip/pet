2ce276975c06c191774c0375092c6619c34217d9
Junip Dewan <junipd2@gmail.com>
2020-05-17 00:38:26 +0530

    Added animate.css

--- src/Pets/Grid.js
+++ src/Pets/Grid.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from "react";
 import GridItem from "./GridItem";
 import pf from "../Api.js";
+import Loader from '../Loader/Loader.js';
 
 const Grid = () => {
+  const [loading, setLoading] =useState(true)
   const [pets, updatePets] = useState([]);
 
   useEffect(() => {
-    pf.animal.search().then(response => {
+    pf.animal.search({type: 'dog', breed: 'akita'}).then(response => {
       updatePets(response.data.animals);
+      setLoading(false)
     });
   }, [pets.length]);
 
   return (
-    <div className="container animals-grid">
-      <div className="grid-boxes">
-        {pets.length > 0 &&
-          pets.map(animal => {
-            return <GridItem animal={animal} key={animal.id} />;
-          })}
-      </div>
-    </div>
-  );
+    <>
+      {loading ? (<div className="spinner"><Loader/></div>) : (
+        <div className="container animals-grid">
+          <div className="grid-boxes">
+            {pets.length > 0 &&
+              pets.map((animal, i) => {
+                return <GridItem cindex={i} animal={animal} key={animal.id}/>
+              })}
+          </div>
+        </div>)
+      }
+    </>
+  )
 };
 
 export default Grid;

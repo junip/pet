e02dc5ab4e3ce6a60a92f41c0bf48caecffa7730
Junip Dewan <junipd2@gmail.com>
2020-05-17 13:30:49 +0530

    implemented infinte scroll

--- src/Pets/Grid.js
+++ src/Pets/Grid.js
@@ -1,35 +1,99 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import GridItem from "./GridItem";
 import pf from "../Api.js";
 import Loader from "../Loader/Loader.js";
 
 const Grid = () => {
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(null);
   const [pets, updatePets] = useState([]);
+  // refs
+  const pageRef = useRef(totalPage);
+  const loadingRef = useRef(loading);
+  const currentPageRef = useRef(currentPage);
+
 
   useEffect(() => {
-    pf.animal.search({ type: "dog", breed: "akita" }).then(response => {
-      updatePets(response.data.animals);
-      setLoading(false);
+    getPets();
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  /**
+   *  Event handling with functional components in react 
+   *  https://medium.com/@rossbulat/react-using-refs-with-the-useref-hook-884ed25b5c29
+   *  
+   */
+
+  // update the total page and reference
+  const updateTotalPage = (data) => {
+    pageRef.current = data
+    setTotalPage(data)
+  }
+
+  // Update loading state
+  const updateLoading = (data) => {
+    loadingRef.current = data 
+    setLoading(data)
+  }
+
+  const updateCurrentPage = (data) => {
+    currentPageRef.current = data 
+    setCurrentPage(data)
+  } 
+
+  /**
+   * concat vs push to update the state using the hooks in react
+   * https://medium.com/@rossbulat/react-using-refs-with-the-useref-hook-884ed25b5c29
+   */
+
+  const getPets = () => {
+    // TODO - need to handle the input from the UI.
+    let searchTerm = { type: "dog", breed: "akita", page: currentPageRef.current };
+    pf.animal.search(searchTerm).then(response => {
+      let petsData = response.data
+      updatePets(pets => [...pets, petsData.animals].flat())
+      updateTotalPage(petsData.pagination.total_pages);
+      updateLoading(false);
     });
-  }, [pets.length]);
+  };
+
+  // implementation of the infinite scroll function
+  // fetch more pets untill the last page
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 300 &&
+      !loadingRef.current && !(currentPageRef.current >= pageRef.current)
+    )  {
+      updateLoading(true);
+      console.log("--CALLED-",  !loadingRef.current)
+      let nextPage = currentPageRef.current + 1;
+      updateCurrentPage(nextPage);
+      getPets();
+      console.log("--CALLED-",  !loadingRef.current)
+    }
+  };
+
+  let loader;
+  if (!pets.length || loading) {
+    loader = (
+      <div className="spinner">
+        <Loader />
+      </div>
+    );
+  }
 
   return (
     <>
-      {loading ? (
-        <div className="spinner">
-          <Loader />
+      <div className="container animals-grid">
+        <div className="grid-boxes">
+          {pets.length > 0 &&
+            pets.map((animal, i) => {
+              return <GridItem cindex={i} animal={animal} key={animal.id} />;
+            })}
         </div>
-      ) : (
-        <div className="container animals-grid">
-          <div className="grid-boxes">
-            {pets.length > 0 &&
-              pets.map((animal, i) => {
-                return <GridItem cindex={i} animal={animal} key={animal.id} />;
-              })}
-          </div>
-        </div>
-      )}
+      </div>
+      {loader}
     </>
   );
 };
